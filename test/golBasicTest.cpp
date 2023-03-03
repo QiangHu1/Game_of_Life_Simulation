c8221a8758f602c4f1141745784924302264b342
Qiang Hu <ucapqhu@ucl.ac.uk>
2023-03-03 03:44:46 +0000

    Add the test case to check the function initialising the grid from a file works

--- test/golBasicTest.cpp
+++ test/golBasicTest.cpp
@@ -51,3 +51,22 @@ TEST_CASE("Check randomly initialising") {
   REQUIRE(num_alive == 12);
 }
 
+TEST_CASE("Check initialising the grid from a file") {
+  Grid gd = *new Grid("../../test/data/glider.txt");
+  REQUIRE(gd.rows == 10);
+  REQUIRE(gd.cols == 10);
+  REQUIRE(gd.getc(1, 2));
+  REQUIRE(gd.getc(2, 2));
+  REQUIRE(gd.getc(3, 2));
+  REQUIRE(gd.getc(2, 0));
+  REQUIRE(gd.getc(3, 1));
+  gd.setc(1, 2, false);
+  gd.setc(2, 2, false);
+  gd.setc(3, 2, false);
+  gd.setc(2, 0, false);
+  gd.setc(3, 1, false);
+  Gameoflife grid = *new Grid(10, 10, 0);
+  bool same = grid.compare(gd);
+  REQUIRE(same);
+}
+
